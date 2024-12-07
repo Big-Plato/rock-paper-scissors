cf2495c457ca1886e0b6140b44bc00ba84f8352e
Big-Plato <luisoctavius.sc@gmail.com>
2024-12-07 20:57:20 -0300

    The game works now. Add 5 rounds

--- index.js
+++ index.js
@@ -7,28 +7,28 @@ let computerScore = 0;
 function getComputerChoice () {
     //Will randomly return one of the string values ("rock", "paper" and "scissors")
     let randomGuess = Math.floor(Math.random() * 3) + 1;
+
+    //if random number is one of the numbers below turn the value of variable to the name of the move
     console.log(randomGuess);
     if (randomGuess === 1) {
-        console.log("I'm random guess: " + randomGuess);
         console.log("Rock");
         randomGuess = "Rock";
     } else if (randomGuess === 2) {
-        console.log("I1m random guesser: " + randomGuess);
         console.log("Paper")
         randomGuess = "Paper";
     } else {
-        console.log(randomGuess);
         console.log("Scissors");
         randomGuess = "Scissors";
     }
 
-
+    //return that name
     return randomGuess;
 }
 
 function getHumanChoice () {
     
     let humanChoice = prompt("What is your choice [Rock, Paper or Scissors]? ");
+
     // Making sure that there is no typo
     if (humanChoice === 'Rock' || humanChoice === 'ROCK' || humanChoice === 'rock') {
         console.log("Rock");
@@ -38,8 +38,9 @@ function getHumanChoice () {
         return "Paper";
     } else if (humanChoice === 'Scissors' || humanChoice === 'SCISSORS' || humanChoice === 'scissors') {
         console.log("Scissors");
-        return "Scissors";
+        return "Human: " + "Scissors";
     } 
+    return "Human's choice was: " + humanChoice;
 
 }
 
@@ -79,8 +80,12 @@ function playRound (getHumanChoice, getComputerChoice) {
 }
 
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+// Iterating over five times through the acquaintance of the results of computer and asking for user's input
+for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
+    playRound(humanSelection, computerSelection);
+}
+
 
